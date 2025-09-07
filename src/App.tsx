be2c37950d82553e545848e17527947f0fdb0ded
2paperstar <lee@paperst.ar>
2025-09-07 14:50:06 +0900

    chore: qna -> faq

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,17 @@ import {
 } from "react-router-dom";
 
 import Layout from "./layout/Layout";
+import FaqDetail from "./pages/faq/DetailFaq.tsx";
+import { FaqLayout } from "./pages/faq/FaqLayout.tsx";
+import Faq from "./pages/faq/FaqList.tsx";
 import OnboardingPage from "./pages/onboarding";
-import FaqDetail from "./pages/qna/DetailFaq.tsx";
-import { FaqLayout } from "./pages/qna/FaqLayout.tsx";
-import QnA from "./pages/qna/FaqList.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<OnboardingPage />} />
       <Route element={<FaqLayout />}>
-        <Route path="/faq" element={<QnA />} />
+        <Route path="/faq" element={<Faq />} />
         <Route path="/faq/:id" element={<FaqDetail />} />
       </Route>
       <Route element={<Layout />}></Route>
